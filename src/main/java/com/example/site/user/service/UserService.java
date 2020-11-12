9a1ef90dc677b66fbe804f463098b009d95b5f19
leeuikang <leeuigang@gmail.com>
2020-11-12 13:34:32 +0900

    user feature make

--- src/main/java/com/example/site/user/service/UserService.java
+++ src/main/java/com/example/site/user/service/UserService.java
@@ -3,8 +3,10 @@ package com.example.site.user.service;
 import com.example.site.user.dto.UserDto;
 import org.springframework.security.core.Authentication;
 
+import javax.security.auth.login.LoginException;
+
 public interface UserService {
-    public void registerUserInfo(UserDto.RegisterUserInfo registerUserInfo);
+    public void registerUserInfo(UserDto.RegisterUserInfo registerUserInfo) throws LoginException;
     public void deleteById(Authentication authentication);
 
 }
--- src/main/java/com/example/site/user/service/UserServiceImpl.java
+++ src/main/java/com/example/site/user/service/UserServiceImpl.java
@@ -3,11 +3,13 @@ package com.example.site.user.service;
 import com.example.site.user.dto.UserDto;
 import com.example.site.user.entity.User;
 import com.example.site.user.repository.UserRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.security.auth.login.LoginException;
 import java.util.Optional;
 
 public class UserServiceImpl implements UserService {
@@ -18,8 +20,19 @@ public class UserServiceImpl implements UserService {
     PasswordEncoder passwordEncoder;
 
     @Override
-    public void registerUserInfo(UserDto.RegisterUserInfo registerUserInfo) {
+    public void registerUserInfo(UserDto.RegisterUserInfo registerUserInfo) throws LoginException {
+        Optional<User> userExisted = userRepository.findByEmail(registerUserInfo.getEmail());
 
+        if(userExisted.isPresent())
+            throw new LoginException();
+
+        String password = passwordEncoder.encode(registerUserInfo.getPassword());
+
+        ModelMapper modelMapper = new ModelMapper();
+        User user = modelMapper.map(registerUserInfo, User.class);
+        user.setPassword(password);
+
+        userRepository.save(user);
     }
 
     @Override
