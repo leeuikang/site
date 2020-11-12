ddfbb42b0b8cf749c516829c30fb3edb1ba8a65a
leeuikang <leeuigang@gmail.com>
2020-11-12 12:32:43 +0900

    devlop-branch

--- src/main/java/com/example/site/SiteApplication.java
+++ src/main/java/com/example/site/SiteApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SiteApplication {
-    
+
     public static void main(String[] args) {
         SpringApplication.run(SiteApplication.class, args);
     }
