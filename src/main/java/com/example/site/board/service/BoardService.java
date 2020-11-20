70b4fa2696a6368659b2580d1329905862730978
leeuikang <leeuigang@gmail.com>
2020-11-20 20:25:06 +0900

    feature - creat local & prod properties files

--- src/main/java/com/example/site/board/service/BoardService.java
+++ src/main/java/com/example/site/board/service/BoardService.java
@@ -1,5 +1,20 @@
 package com.example.site.board.service;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
 public interface BoardService {
-    
+
+    Map<String, Object> selectBoardList(Map<String, Object> map) throws Exception;
+
+    Map<String, Object> selectBoardSearchList(Map<String, Object> map) throws Exception;
+
+    Map<String, Object> selectBoardDetails(Map<String, Object> map) throws Exception;
+
+    void insertBoard(Map<String, Object> map, HttpServletRequest request) throws Exception;
+
+    boolean updateBoard(Map<String, Object> map, HttpServletRequest request) throws Exception;
+
+    boolean deleteBoard(Map<String, Object> map, HttpServletRequest request) throws Exception;
+
 }
--- src/main/java/com/example/site/board/service/BoardServiceImpl.java
+++ src/main/java/com/example/site/board/service/BoardServiceImpl.java
@@ -2,6 +2,40 @@ package com.example.site.board.service;
 
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
 @Service
 public class BoardServiceImpl implements BoardService{
+
+    @Override
+    public Map<String, Object> selectBoardList(Map<String, Object> map) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> selectBoardSearchList(Map<String, Object> map) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> selectBoardDetails(Map<String, Object> map) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void insertBoard(Map<String, Object> map, HttpServletRequest request) throws Exception {
+
+    }
+
+    @Override
+    public boolean updateBoard(Map<String, Object> map, HttpServletRequest request) throws Exception {
+        return false;
+    }
+
+    @Override
+    public boolean deleteBoard(Map<String, Object> map, HttpServletRequest request) throws Exception {
+        return false;
+    }
+
 }
